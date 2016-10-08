cf869e3291299c6a4d57cbc3e69f45b1bfc01992
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-08 14:19:57 -0500

    php test

--- CreateUser.php
+++ CreateUser.php
@@ -1,14 +1,17 @@
 <?php
 $username = $_POST["username"];
 
-if($username = "")
+echo "Name: " . $username;
+
+/*
+if($username = ""){
   echo "Could not create new user, no username was entered.";
+}
 //elseif ( username is already in table)
 //  echo "Could not create new user, username already taken.";
 else {
   //add user to table
   echo "User " . $username . " successfully created.";
 }
-
-echo $username;
+*/
 ?>
