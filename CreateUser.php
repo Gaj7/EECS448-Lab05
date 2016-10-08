638b161643e1960cb97898c987324fba1352f3dc
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-08 15:07:00 -0500

    added mysql interaction to php backend

--- CreateUser.php
+++ CreateUser.php
@@ -1,15 +1,56 @@
 <?php
 $username = $_POST["username"];
-
 if($username == ""){
   echo "Could not create new user, no username was entered.";
 }
-else {
-  echo "User " . $username . " successfully created.";
+else{
+
+  $mysqli = new mysqli("https://mysql.eecs.ku.edu", "gjurgensen", "Password123!2", "gjurgensen");
+  /* check connection */
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+
+  //$query = "SELECT user_id FROM Users";
+  $nameIsUnique = true;
+  if($result = $mysqli->query("SELECT user_id FROM Users")){
+    while ($row = $result->fetch_assoc()) {
+        //printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+        echo $row["user_id"] . "<br>";
+        $nameIsUnique = false;
+      }
+    $result->free();
+  }
+
+  if($nameIsUnique){
+    if ($mysqli->query("INSERT INTO Users (user_id) VALUES ($username)") === TRUE) {
+      echo "User " . $username . " successfully created.";
+    }
+    else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    //echo "User " . $username . " successfully created.";
+  }
+  else
+    echo "Could not create new user, username already taken.";
 }
 
+
+
+
+
+// $username = $_POST["username"];
+//
+// if($username == ""){
+//   echo "Could not create new user, no username was entered.";
+// }
+// else {
+//   echo "User " . $username . " successfully created.";
+// }
+
 /*
-if($username = ""){
+if($username == ""){
   echo "Could not create new user, no username was entered.";
 }
 //elseif ( username is already in table)
@@ -19,4 +60,5 @@ else {
   echo "User " . $username . " successfully created.";
 }
 */
+
 ?>
