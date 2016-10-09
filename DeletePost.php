b8ad091ffcd9e175157a10ccb421958d261b295e
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-09 00:00:10 -0500

    started delete post backed

---
+++ DeletePost.php
@@ -0,0 +1,14 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "gjurgensen", "Password123!2", "gjurgensen");
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connection failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+if($result = $mysqli->query("SELECT post_id FROM Posts")){
+  while ($row = $result->fetch_assoc()) {
+    echo "test: " . $_POST[$row["post_id"]] . "<br>";
+  }
+}
+?>
