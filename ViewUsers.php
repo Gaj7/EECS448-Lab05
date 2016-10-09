3d5ff83157e15e87652d9ec1af607108670cd6dc
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-08 23:47:24 -0500

    added DeletePost page

--- ViewUsers.php
+++ ViewUsers.php
@@ -8,7 +8,7 @@ if ($mysqli->connect_errno) {
 
 $result = $mysqli->query("SELECT user_id FROM Users");
 if($result->num_rows > 0){
-  echo "<table><tr><td>user_id</td></tr>";
+  echo "<table><tr><td>user_id</td></tr><tr></tr>";
   while ($row = $result->fetch_assoc()) {
     echo "<tr><td>" . $row["user_id"] . "</td></tr>";
   }
