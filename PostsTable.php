96d6727ecd4e0f7af452b69eb5498740d5c3ee13
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-08 23:50:44 -0500

    another bug fix

--- PostsTable.php
+++ PostsTable.php
@@ -10,7 +10,8 @@ $result = $mysqli->query("SELECT post_id, content, author_id FROM Posts");
 if($result->num_rows > 0){
   echo "<table><tr><td>post_id</td><td>content</td><td>author_id</td><td>Delete?</td></tr><tr></tr>";
   while ($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["post_id"] . "</td><td>" . $row["content"] . "</td><td>" . $row["author_id"] ."</td><input type='checkbox' name='delete' value='" . $row["post_id"] . "></input>I have a bike<br></tr>";
+    echo "<tr><td>" . $row["post_id"] . "</td><td>" . $row["content"] . "</td><td>" . $row["author_id"]
+      . "</td><td><input type='checkbox' name='delete' value='" . $row["post_id"] . "></input></td></tr>";
   }
   echo "</table>";
   $result->free();
