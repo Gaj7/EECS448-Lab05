89cb61ff1be824cc827724e41f2b3e0160a4e9e2
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-09 00:12:52 -0500

    final functionality on delete posts backend

--- DeletePost.php
+++ DeletePost.php
@@ -8,8 +8,12 @@ if ($mysqli->connect_errno) {
 
 if($result = $mysqli->query("SELECT post_id FROM Posts")){
   while ($row = $result->fetch_assoc()) {
-    if($_POST[$row["post_id"]] == "y")
-      echo $row["post_id"] . "<br>";
+    if($_POST[$row["post_id"]] == "y"){
+      if($mysqli->query("DELETE FROM Posts WHERE post_id=" . $row['post_id']))
+        echo "post_id: " . $row["post_id"] . "successfully deleted.";
+      else
+        echo "post_id: " . $row["post_id"] . "failed to delet.";
+    }
   }
 }
 ?>
