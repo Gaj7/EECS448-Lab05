dae4daec26f2db2d01c3f2ef841221da4441ed3f
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-18 13:34:04 -0500

    improved PostsTable for empty table

--- PostsTable.php
+++ PostsTable.php
@@ -8,7 +8,8 @@ if ($mysqli->connect_errno) {
 
 $result = $mysqli->query("SELECT post_id, content, author_id FROM Posts");
 if($result->num_rows > 0){
-  echo "<table><tr><td>post_id</td><td>content</td><td>author_id</td><td>Delete?</td></tr><tr></tr>";
+  echo "<table cellpadding='10'>";
+  echo "<tr><td>post_id</td><td>content</td><td>author_id</td><td>Delete?</td></tr><tr></tr>";
   while ($row = $result->fetch_assoc()) {
     echo "<tr><td>" . $row["post_id"] . "</td><td>" . $row["content"] . "</td><td>" . $row["author_id"]
       . "</td><td><input type='checkbox' name='" . $row["post_id"] . "' value='y'></input></td></tr>";
@@ -17,6 +18,6 @@ if($result->num_rows > 0){
   $result->free();
 }
 else{
-  echo "No posts have been made by that user";
+  echo "No posts exist for any user.<br>";
 }
 ?>
